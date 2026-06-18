1b187ce0767faa06fa9f3fda67bc045a6dfcc8d5
hohasic <hohasic@gmail.com>
2026-06-18 10:48:55 +0900

    member

--- 20260618ex/ex001/app.py
+++ 20260618ex/ex001/app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# signup_form
+@app.route('/member/signup_form', methods=['GET'])
+def signup_form():
+    return render_template('signup_form.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ 20260618ex/ex001/static/js/member.js
@@ -0,0 +1,4 @@
+function signupForm() {
+    console.log('signupForm() CALLED!!');
+    
+}
